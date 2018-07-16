1e257a6fa60e96caa89127bbfeb5182a463404e7
Cyril Pernet <wamcyril@gmail.com>
2018-07-16 12:41:28 +0200

    full repo commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Brain_colours
+
+## background
+
+Colour maps used in scientific litterature must reflect the underlying data. The problem with many maps is that while underlying values are represented by different colours (as red-green-blue values) they are not perceived as such. This is explained by the fact that human do not perceive colours in a uniform fashion, and that most colour maps have colour and luminance confounded. One solution is the make map in CIELAB (Luminance A:red-green B:bleu-yellow) space.
+
+## data ad code
+
+This repository contains the Malab code used to generate colour maps as well as the maps as .csv, .mat and .lut files
+Brain colours data are made available under the [Open Data Commons Attribution License](http://opendatacommons.org/licenses/by/1.0).
+
+## dependencies
+
+ Initial colour maps are taken from [MRICro](https://www.nitrc.org/projects/mricron)
+.lut fules are read using Matthew Brett's pr_getcmap.m function (slover in [SPM](https://www.fil.ion.ucl.ac.uk/spm/))
+The luminance is fixed using [equalisecolourmap.m]https://www.peterkovesi.com/matlabfns/index.html#colour and related files provided by Peter Kovesi (Peter Kovesi. Good Colour Maps: How to Design Them. arXiv:1509.03700). Note this also depends on [deltaE2000.m](http://www2.ece.rochester.edu/~gsharma/ciede2000/).
+The vizualization is done using are RGB to LAB space converion using the colorspace.m and colormapline.m function from [Matteo Niccoli](https://mycarta.wordpress.com/2012/05/12/the-rainbow-is-dead-long-live-the-rainbow-part-1/)
---
+++ code/map_luminance.m
@@ -0,0 +1,71 @@
+% This is the matlab script used to 'fix' the luminance issue in colour
+% maps used in human brain tomographic imaging
+%
+% - initial colour maps taken from MRICro (https://www.nitrc.org/projects/mricron)
+% - .lut fules are read using Matthew Brett's pr_getcmap.m function (slover)
+% - the luminance is fixed using equalisecolourmap.m provided by Peter Kovesi
+% (Peter Kovesi. Good Colour Maps: How to Design Them. arXiv:1509.03700)
+% https://www.peterkovesi.com/matlabfns/index.html#colour
+% also depends on http://www2.ece.rochester.edu/~gsharma/ciede2000/
+% - the vizualization is done using are RGB to LAB space converion using the
+% colorspace.m and colormapline.m function from Matteo Niccoli
+% (https://mycarta.wordpress.com/2012/05/12/the-rainbow-is-dead-long-live-the-rainbow-part-1/)
+%
+% Cyril Pernet 16 July 2018
+
+current = pwd;
+%% scan for colour map taken from mricro ()
+
+cd('D:\MRI\mricron\lut')
+files = dir('*.lut');
+
+%% now read, correct luminance, and save
+cd(current);
+mkdir('new_braincolour_maps')
+
+for map = 1:size(files,1)
+    lutmap = pr_getcmap([files(map).folder filesep files(map).name]);
+    try
+        lutmap2 = equalisecolourmap('RGB', lutmap, 'CIE76', [1 0 0], 1/25*length(lutmap), 0,0);
+        csvwrite(['new_braincolour_maps' filesep files(map).name(1:end-4) '.csv'],lutmap2)
+        save(['new_braincolour_maps' filesep files(map).name(1:end-4)],'lutmap2')
+        map2ermapperlutfile(lutmap2, ['new_braincolour_maps' filesep files(map).name(1:end-4)], files(map).name(1:end-4))
+
+        lutmap2 = equalisecolourmap('RGB', lutmap, 'CIE76', [1 1 1], 1/25*length(lutmap), 0,0);
+        csvwrite(['new_braincolour_maps' filesep files(map).name(1:end-4) '_iso.csv'],lutmap2)
+        save(['new_braincolour_maps' filesep files(map).name(1:end-4) '_iso'],'lutmap2')
+        map2ermapperlutfile(lutmap2, ['new_braincolour_maps' filesep files(map).name(1:end-4) '_iso'], files(map).name(1:end-4),'isoluminant')
+    end
+end
+
+%% add two diverging colour maps
+[dmap, name, desc] = cmap('D1');
+csvwrite(['new_braincolour_maps' filesep 'diverging_bwr.csv'],dmap)
+save(['new_braincolour_maps' filesep 'diverging_bwr'],'dmap')
+map2ermapperlutfile(dmap, ['new_braincolour_maps' filesep 'diverging_bwr'], 'diverging_bwr', desc)
+
+[dmap, name, desc] = cmap('D7');
+csvwrite(['new_braincolour_maps' filesep 'diverging_bgy.csv'],dmap)
+save(['new_braincolour_maps' filesep 'diverging_bgy'],'dmap')
+map2ermapperlutfile(dmap, ['new_braincolour_maps' filesep 'diverging_bgy'], 'diverging_bgy', desc)
+
+%% make a figure to illustrate
+figure; index =1;
+for map = [1  10 12 13]
+    cd D:\MRI\SPM12\@slover\private
+    lutmap = pr_getcmap([files(map).folder filesep files(map).name]);
+    cd(current)
+    lutmap2 = equalisecolourmap('RGB', lutmap, 'CIE76', [1 0 0], 1/25*length(lutmap), 0,0);
+    subplot(2,4,index)
+    LS=colorspace('RGB->Lab',lutmap);
+    h=colormapline(1:1:length(LS),LS(:,1),[],lutmap);
+    set(h,'linewidth',2); grid on; box on; axis square;
+    ylabel('Luninance')
+    title ([files(map).name(1:end-4)],'Color','k','FontSize',12);
+    subplot(2,4,index+4);
+    LS=colorspace('RGB->Lab',lutmap2);
+    h=colormapline(1:1:length(LS),LS(:,1),[],lutmap2);
+    set(h,'linewidth',2); grid on; box on; axis square;
+    xlabel('Colour level'); ylabel('Luninance')
+    index = index+1;
+end
